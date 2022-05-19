0bcf317232184ef41524a732ef2408399889f236
vsuyazov <suiazov.vlad@gmail.com>
2022-05-19 12:21:00 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        GameProgress player1 = new GameProgress(100, 2, 5, 1500);
+        GameProgress player2 = new GameProgress(90, 3, 7, 2500);
+        GameProgress player3 = new GameProgress(150, 5, 15, 8000);
+
+        saveGame("C://Users//stix_//Desktop//Games//savegames//save1.dat", player1);
+        saveGame("C://Users//stix_//Desktop//Games//savegames//save2.dat", player2);
+        saveGame("C://Users//stix_//Desktop//Games//savegames//save3.dat", player3);
+
+        File file = new File("C://Users//stix_//Desktop//Games//savegames");
+
+        zipFiles("C://Users//stix_//Desktop//Games//savegames//save.zip", file);
+
+    }
+
+    public static void saveGame(String path, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(path);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void zipFiles(String path, File file) {
+        for (File item : Objects.requireNonNull(file.listFiles())) {
+            try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path, true));
+                 FileInputStream fis = new FileInputStream(item)) {
+                ZipEntry entry = new ZipEntry(item.getName());
+                zout.putNextEntry(entry);
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer);
+                zout.write(buffer);
+                zout.closeEntry();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
